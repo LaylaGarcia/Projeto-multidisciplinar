552e6eccd926ed31fd61cd0d9e04526f931fb42a
LaylaGarcia <laylagarciaifsp@hotmail.com>
2015-10-27 13:08:00 -0200

    Update banco.sql

--- Banco-de-dados/banco.sql
+++ Banco-de-dados/banco.sql
@@ -1,1 +1,14 @@
-
+CREATE TABLE func(
+	nome varchar(20),
+	cpf int,
+	rg varchar(9),
+	numero int,
+	setor varchar(10),
+	estadocivil varchar(20),
+	salario int,
+	gestor varchar(20),
+	cargo varchar(10),
+	datanascimento date,
+	endereco varchar(20),
+	PRIMARY KEY (cpf)
+);
